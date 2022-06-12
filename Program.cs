850d84199276b1bdb16750b2acfb2eb31b283699
mbyrne95 <76924518+mbyrne95@users.noreply.github.com>
2022-06-12 16:04:41 -0400

    basic setup

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace DestinyBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = System.IO.File.ReadAllText("token.txt"),
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged
+            });
+        }
+    }
+}
